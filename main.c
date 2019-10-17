be9c001b1d6bb79d7805bd8bdc6bedf4761b4255
vizhyk <vizhyk1@gmail.com>
2019-10-17 15:41:39 +0300

    Lesson 1: Implemented leds "heartbeat"
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

--- main.c
+++ main.c
@@ -94,7 +94,22 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+	   HAL_Delay(400);
+	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+	   HAL_Delay(400);	
   }
   /* USER CODE END 3 */
 }
