00b0199d26e3a890aa840dc09f4a3601d70caeea
two-dimensional-array <andrii.kolomiiets@nure.ua>
2019-10-18 23:19:25 +0300

    Topic: Feature. Action: Added LED blinking.

--- 1/Src/main.c
+++ 1/Src/main.c
@@ -115,6 +115,10 @@ int main(void)
     MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
+    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+    HAL_Delay(500);
+    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+    HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
