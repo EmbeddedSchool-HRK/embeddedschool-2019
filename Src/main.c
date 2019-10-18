c83b3d2f6c8c0d5f083de53768ed2eab69927f2d
Morok <anknowntitov@gmail.com>
2019-10-18 16:51:49 +0300

    Lesson #1 Function for blinking LEDs cross to cross
    Creat function for blinking LEDs cross to cross
    
    Signed-off-by: Morok <anknowntitov@gmail.com>

--- Src/main.c
+++ Src/main.c
@@ -65,7 +65,10 @@ void MX_USB_HOST_Process(void);
 
 /* USER CODE BEGIN PFP */
 
-  void Burning_all_LEDs();
+
+void Burning_all_LEDs();
+void Blinking_LEDs_crossTOcross();
+
 
 /* USER CODE END PFP */
 
@@ -124,6 +127,26 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
+  Blinking_LEDs_crossTOcross();
+
+  }
+  /* USER CODE END 3 */
+}
+
+
+	/* USER CODE BEGIN 3 */
+void Burning_all_LEDs() {
+  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LD5_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+  HAL_Delay(5000);
+  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+}
+void Blinking_LEDs_crossTOcross() {
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
   HAL_Delay(500);
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
@@ -143,26 +166,6 @@ int main(void)
 
  }
 
-  
-}
-  }
-
-  /* USER CODE END 3 */
-
- 
-  }
-
-void Burning_all_LEDs() {
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-  HAL_Delay(5000);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-}
 
 
 /**
