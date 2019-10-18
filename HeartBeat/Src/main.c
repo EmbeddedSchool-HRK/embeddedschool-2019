af5fbd22c24bc51f85057441f6ce3d99f184e11d
Tanya Litvinenko <tetiana.lytvynenko@nure.ua>
2019-10-18 15:41:40 +0300

    Lesson #1: orange LED
    Function HeartBeat for orange LED.
    
    Signed-off-by: Tanya Litvinenko <tetiana.lytvynenko@nure.ua>

--- HeartBeat/Src/main.c
+++ HeartBeat/Src/main.c
@@ -116,11 +116,17 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
+
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
   HAL_Delay(5000);
+  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+  HAL_Delay(500);
+
 
   }
   /* USER CODE END 3 */
