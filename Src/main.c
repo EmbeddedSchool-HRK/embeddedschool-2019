9793310a132e9ab6844db9828902c3155bc3f8f4
Morok <anknowntitov@gmail.com>
2019-10-18 16:45:15 +0300

    Blinking LEDs cros to cross stage-4

--- Src/main.c
+++ Src/main.c
@@ -119,6 +119,25 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 		Burning_all_LEDs();
+		while (1)
+		    {
+		 	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
+		 	HAL_Delay(500);
+		 	HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+		  	HAL_Delay(500);
+		  }
   }
   /* USER CODE END 3 */
 }
