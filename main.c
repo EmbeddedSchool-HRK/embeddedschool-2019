315108472638956f4d88e34be25abb855059f00a
vizhyk <vizhyk1@gmail.com>
2019-11-01 16:19:16 +0200

    Lesson 1: Implemented blinking function.
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

--- main.c
+++ main.c
@@ -63,7 +63,26 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
-
+void Heartbeat(){
+	
+	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+	   HAL_Delay(400);
+	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(400);	
+	
+	};
 
 int main(void)
 {
@@ -98,23 +117,7 @@ int main(void)
   while (1)
   {
 
-	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-	   HAL_Delay(100);
-	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-	   HAL_Delay(100);
-	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
-	   HAL_Delay(200);
-	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-	   HAL_Delay(200);
-	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-	   HAL_Delay(300);
-	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-	   HAL_Delay(300);
-	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
-	   HAL_Delay(400);
-	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-	   HAL_Delay(400);	
-	
+	   Heartbeat();
   }
   /* USER CODE END 3 */
 }
