b3294be32857b6cb53f9d7292de2a2e388425b9b
vizhyk <vizhyk1@gmail.com>
2019-11-01 15:15:24 +0200

    Lesson 1: Implemented leds blinking.
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

--- main.c
+++ main.c
@@ -97,6 +97,22 @@ int main(void)
   while (1)
   {
 
+	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(100);
+	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(200);
+	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(300);
+	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+	   HAL_Delay(400);
+	   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+	   HAL_Delay(400);	
   }
   /* USER CODE END 3 */
 }
