23d14248a55a325d8ec2d3c1235fec2ff5385cd2
sergii.aksonov <sergii.aksonov@globallogic.com>
2019-11-08 12:41:55 +0200

    Lesson 1: Add blinking by LED
    Add blinking by LED in the main loop.
    
    Signed-off-by: sergii.aksonov <sergii.aksonov@globallogic.com>

--- Src/main.c
+++ Src/main.c
@@ -3,6 +3,9 @@
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
+  * @author         : Aksonov Sergii
+  * @description    : Source code developed in scope of the course
+  *                   Embedded Software Design
   ******************************************************************************
   * @attention
   *
@@ -34,6 +37,9 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
+#define PERIOD 500
+#define DUTY_CYCLE 100
 
 /* USER CODE END PD */
 
@@ -115,6 +121,11 @@ int main(void)
     MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
+    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+    HAL_Delay(DUTY_CYCLE);
+    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+    HAL_Delay(PERIOD - DUTY_CYCLE);
+
   }
   /* USER CODE END 3 */
 }
