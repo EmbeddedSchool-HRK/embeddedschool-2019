948d9dd973cf4c6b1ab53f2ece487df130bbe7c6
Morok <anknowntitov@gmail.com>
2019-11-10 16:54:16 +0200

    Lesson 2. Driver of LEDs
    Writing a driver to facilitate readability and code building
    
    Signed-off-by: Morok <anknowntitov@gmail.com>

---
+++ Src/Drv_LEDs.c
@@ -0,0 +1,35 @@
+/*
+ * Drv_LEDs.c
+ *
+ *  Created on: 10 нояб. 2019 г.
+ *      Author: morok__
+ */
+
+#include "Drv_LEDs.h"
+#include "main.h"
+
+
+typedef struct {
+  GPIO_TypeDef* port;
+  uint16_t pin;
+}LED_type;
+
+LED_type leds[LED_COUNT] = {
+  {LD6_GPIO_Port, LD6_Pin},  // LED_BLUE
+  {LD5_GPIO_Port, LD5_Pin},  // LED_RED
+  {LD3_GPIO_Port, LD3_Pin},  // LED_ORANGE
+  {LD4_GPIO_Port, LD4_Pin},  // LED_GREEN
+
+};
+
+void DrvLEDs_on(LED_LIST ledName){
+
+  HAL_GPIO_WritePin(leds[ledName].port, leds[ledName].pin, GPIO_PIN_SET);
+
+}
+
+void DrvLEDs_off(LED_LIST ledName){
+
+  HAL_GPIO_WritePin(leds[ledName].port, leds[ledName].pin, GPIO_PIN_RESET);
+
+}
