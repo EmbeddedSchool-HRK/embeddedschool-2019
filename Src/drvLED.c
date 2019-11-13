d5e9c695d4a3c6869c0c05fcafabf9ff69fa3be7
Violetta2525 <heletto.violetta@nure.ua>
2019-11-13 17:11:21 +0200

    Lesson #2: Heletto Violetta
    Drivers is ready

--- Src/drvLED.c
+++ Src/drvLED.c
@@ -6,4 +6,18 @@
  */
 
 #include "drvLED.h"
+#include "main.h"
 
+void drvLeds_ON(led_t LEDsColour)
+{
+	HAL_GPIO_WritePin(GPIOD, LEDsColour, GPIO_PIN_SET);
+}
+
+void drvLeds_OFF(led_t LEDsColour)
+{
+	HAL_GPIO_WritePin(GPIOD, LEDsColour, GPIO_PIN_RESET);
+}
+ void Delay(int Time)
+ {
+	 HAL_Delay(Time);
+ }
