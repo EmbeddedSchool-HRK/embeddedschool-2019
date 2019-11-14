8302c3eaed9751bbbce8bfc01ae3a9ee96cc11cf
illia-lykhoshvai <illia.lykhoshvai@nure.ua>
2019-11-14 22:25:21 +0200

    Lesson#2: Modified driver Heartbeat.c
    - [x] Modified driver Heartbeat.c to enter LED sequence via array.
    
    Signed-off-by: lykhoshvai-illia <illialykhoshvai@gmail.com>

--- lessons/Src/heartbeat.c
+++ lessons/Src/heartbeat.c
@@ -9,22 +9,16 @@
 #include "heartbeat.h"
 #include "drvLeds.h"
 
+ledsList_t LED_array [LED_AMOUNT] = {LED_ORANGE, LED_BLUE, LED_RED,LED_GREEN};
+// здесь вводить последовательность светодиодов
+
+
 void Heartbeat_run(uint16_t dutyCycle, uint16_t period)
 {
-  drvLeds_on(LED_ORANGE);
+  for (uint16_t ledName = 0; ledName < LED_AMOUNT - 1 ; ledName++) {
+  drvLeds_on(LED_array[ledName]);
   HAL_Delay(dutyCycle);
-  drvLeds_off(LED_ORANGE);
+  drvLeds_off(LED_array[ledName]);
   HAL_Delay(period - dutyCycle);
-  drvLeds_on(LED_BLUE);
-  HAL_Delay(dutyCycle);
-  drvLeds_off(LED_BLUE);
-  HAL_Delay(period - dutyCycle);
-  drvLeds_on(LED_RED);
-  HAL_Delay(dutyCycle);
-  drvLeds_off(LED_RED);
-  HAL_Delay(period - dutyCycle);
-  drvLeds_on(LED_GREEN);
-  HAL_Delay(dutyCycle);
-  drvLeds_off(LED_GREEN);
-  HAL_Delay(period - dutyCycle);
+  }
 }
