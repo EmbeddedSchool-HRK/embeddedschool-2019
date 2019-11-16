39d5f3468e6cd65cfe2dee19662a2e27471b965c
S1ckret <S1ckret@users.noreply.github.com>
2019-11-16 16:34:57 +0200

    Lesson 3: Add ledsQueue via ulSysTime
    NonBlocking ledsQueue is added.
    
    Signed-off-by: S1ckret <S1ckret@users.noreply.github.com>

--- Core/Src/ledsQueue.c
+++ Core/Src/ledsQueue.c
@@ -7,6 +7,7 @@
 #include "stm32f4xx_hal.h"
 
 #include "ledsQueue.h"
+#include "ulSysTime.h"
 
 void LedsQueue_default(uint32_t time)
 {
@@ -28,12 +29,80 @@ void LedsQueue_default(uint32_t time)
 }
 
 
+typedef enum
+{
+	STATE_TURN_LED_ON,
+	STATE_DUTYCYCLE,
+	STATE_TURN_LED_OFF,
+	STATE_INCREMENT
+} ledsQueueState_t;
+
+
+static ledsQueueState_t StateTurnLedOn(uint32_t* startTime, const led_color_t led_color);
+
+static ledsQueueState_t StateDutycycle(const ledsQueueState_t currentState, uint32_t startTime, uint32_t nextTime);
+
+static ledsQueueState_t StateTurnLedOff(uint32_t* startTime, const led_color_t led_color);
+
+static ledsQueueState_t StateIncrement(uint32_t* currentIndex, uint32_t maxIndex);
+
 void LedsQueue(led_color_t* leds_queue, uint32_t count, uint32_t time)
 {
-	for (uint32_t i = 0; i < count; i++)
+	static ledsQueueState_t currentState = STATE_TURN_LED_ON;
+	static ledsQueueState_t nextState = STATE_DUTYCYCLE;
+
+	static uint32_t startTime = 0;
+	static uint32_t currentLedIndex = 0;
+	switch (currentState)
 	{
-		drvLed_on(leds_queue[i]);
-		HAL_Delay(time);
-		drvLed_off(leds_queue[i]);
+	case STATE_TURN_LED_ON :
+		nextState = StateTurnLedOn(&startTime, leds_queue[currentLedIndex]);
+		break;
+	case STATE_DUTYCYCLE :
+		nextState = StateDutycycle(currentState, startTime, time);
+		break;
+	case STATE_TURN_LED_OFF :
+		nextState = StateTurnLedOff(&startTime, leds_queue[currentLedIndex]);
+		break;
+	case STATE_INCREMENT :
+		nextState = StateIncrement(&currentLedIndex, count);
+		break;
+	default:
+		nextState = STATE_TURN_LED_ON;
 	}
+
+	currentState = nextState;
 }
+
+
+static ledsQueueState_t StateTurnLedOn(uint32_t* startTime, const led_color_t led_color)
+{
+	drvLed_on(led_color);
+	*startTime = ulSysTime_GetCurrentTime();
+	return STATE_DUTYCYCLE;
+}
+
+static ledsQueueState_t StateDutycycle(const ledsQueueState_t currentState, uint32_t startTime, uint32_t nextTime)
+{
+	uint8_t isItTime = ulSysTime_IsItTime(startTime, nextTime);
+	ledsQueueState_t nextState = currentState;
+	if (isItTime)
+	{
+		nextState = STATE_TURN_LED_OFF;
+	}
+	return nextState;
+}
+
+static ledsQueueState_t StateTurnLedOff(uint32_t* startTime, const led_color_t led_color)
+{
+	drvLed_off(led_color);
+	*startTime = ulSysTime_GetCurrentTime();
+	return STATE_INCREMENT;
+}
+
+static ledsQueueState_t StateIncrement(uint32_t* currentIndex, uint32_t maxIndex)
+{
+	*currentIndex = ++(*currentIndex) % maxIndex;
+	return STATE_TURN_LED_ON;
+}
+
