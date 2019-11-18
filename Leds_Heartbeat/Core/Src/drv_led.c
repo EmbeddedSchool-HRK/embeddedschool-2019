8efd29c88b896bd278a8d48dfed6ac2edda584bc
vizhyk <vizhyk1@gmail.com>
2019-11-19 00:16:55 +0200

    Lesson 2: Implemented Led driver.
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

---
+++ Leds_Heartbeat/Core/Src/drv_led.c
@@ -0,0 +1,30 @@
+/*
+ * drv_led.c
+ *
+ *  Created on: 18 нояб. 2019 г.
+ *      Author: vit
+ */
+#include "main.h"
+#include "drv_led.h"
+
+
+typedef struct{
+	GPIO_TypeDef* port;
+	uint16_t pin;
+}led_type;
+
+led_type array[Counter] =
+{
+	{GPIOD, GPIO_PIN_12},
+	{GPIOD, GPIO_PIN_13},
+	{GPIOD, GPIO_PIN_14},
+	{GPIOD, GPIO_PIN_15}
+};
+
+void Led_On(led_colours led_number){
+  HAL_GPIO_WritePin(array[led_number].port, array[led_number].pin, GPIO_PIN_SET);
+}
+
+void Led_Off(led_colours led_number){
+  HAL_GPIO_WritePin(array[led_number].port, array[led_number].pin, GPIO_PIN_RESET);
+}
