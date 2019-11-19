b43b84cd16f737f0b0ab51a653094cc39bf19323
vizhyk <vizhyk1@gmail.com>
2019-11-19 02:22:03 +0200

    Lesson 2: Created array for moving leds.
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

--- Leds_Heartbeat/Core/Src/heartbeat_module.c
+++ Leds_Heartbeat/Core/Src/heartbeat_module.c
@@ -8,29 +8,31 @@
 #include "main.h"
 #include "drv_led.h"
 
-void heartbeat_moving(int duty_cycle){
-	Leds_activation(Led_Green);
-	HAL_Delay(duty_cycle);
-	Leds_disable(Led_Green);
-	Leds_activation(Led_Orange);
-	HAL_Delay(duty_cycle);
-	Leds_disable(Led_Orange);
-	Leds_activation(Led_Red);
-	HAL_Delay(duty_cycle);
-	Leds_disable(Led_Red);
-	Leds_activation(Led_Blue);
-	HAL_Delay(duty_cycle);
-	Leds_disable(Led_Blue);
-}
+led_colour mass[Counter] =
+{
+		Led_Green,
+		Led_Orange,
+		Led_Red,
+		Led_Blue,
+};
 
+void heartbeat_moving(int duty_cycle){
+	for(led_colour colour = 0; colour < Counter; colour++){
+		  Leds_activation(mass[colour]);
+		  HAL_Delay(duty_cycle);
+		  Leds_disable(mass[colour]);
+		  HAL_Delay(duty_cycle);
+}
+}
 void all_leds_blinking(int dual_duty_cycle){
 	for(led_colour colour = 0; colour < Counter; colour++){
 	Leds_activation(colour);
-	}
+		}
 
 	HAL_Delay(dual_duty_cycle);
 	for(led_colour colour = 0; colour < Counter; colour++){
 	Leds_disable(colour);
-	}
-
+		}
 }
+
+
