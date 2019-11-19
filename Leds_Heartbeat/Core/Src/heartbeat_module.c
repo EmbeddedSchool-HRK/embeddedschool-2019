9f080268bbcc0eb1c028a6c4d086b545b7c9dbb6
vizhyk <vizhyk1@gmail.com>
2019-11-19 02:08:21 +0200

    Lesson 2: Implemented moving leds module.
    Signed-off-by: vizhyk <vizhyk1@gmail.com>

--- Leds_Heartbeat/Core/Src/heartbeat_module.c
+++ Leds_Heartbeat/Core/Src/heartbeat_module.c
@@ -4,13 +4,33 @@
  *  Created on: 19 нояб. 2019 г.
  *      Author: vit
  */
+#include "heartbeat_module.h"
 #include "main.h"
 #include "drv_led.h"
-#include "hearbeat_module.h"
 
-void heartbeat_func (led_colour colour,int duty_cycle){
-  Leds_activation(colour);
-  HAL_Delay(duty_cycle);
-  Leds_disable(colour);
-  HAL_Delay(duty_cycle);
+void heartbeat_moving(int duty_cycle){
+	Leds_activation(Led_Green);
+	HAL_Delay(duty_cycle);
+	Leds_disable(Led_Green);
+	Leds_activation(Led_Orange);
+	HAL_Delay(duty_cycle);
+	Leds_disable(Led_Orange);
+	Leds_activation(Led_Red);
+	HAL_Delay(duty_cycle);
+	Leds_disable(Led_Red);
+	Leds_activation(Led_Blue);
+	HAL_Delay(duty_cycle);
+	Leds_disable(Led_Blue);
+}
+
+void all_leds_blinking(int dual_duty_cycle){
+	for(led_colour colour = 0; colour < Counter; colour++){
+	Leds_activation(colour);
+	}
+
+	HAL_Delay(dual_duty_cycle);
+	for(led_colour colour = 0; colour < Counter; colour++){
+	Leds_disable(colour);
+	}
+
 }
