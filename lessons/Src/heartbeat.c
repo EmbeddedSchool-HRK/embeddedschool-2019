518bac9f97bff9ee372df0dd9e051a622636e93c
illia-lykhoshvai <illia.lykhoshvai@nure.ua>
2019-11-21 00:27:14 +0200

    Lesson#3: Reworked heartbeat.c using ulSysTime
    - [x] Reworked heartbeat.c using ulSysTime with switch-case method.
    
    Signed-off-by: illia-lykhoshvai <illia.lykhoshvai@nure.ua>

--- lessons/Src/heartbeat.c
+++ lessons/Src/heartbeat.c
@@ -9,16 +9,58 @@
 #include "heartbeat.h"
 #include "drvLeds.h"
 
-ledsList_t LED_array [LED_AMOUNT] = {LED_ORANGE, LED_BLUE, LED_RED,LED_GREEN};
+ledsList_t LED_array = {LED_BLUE};
 // здесь вводить последовательность светодиодов
 
+typedef enum{
+  STATE_TURN_LED_ON,
+  STATE_WAIT_DUTYCYCLE,
+  STATE_TURN_LED_OFF,
+  STATE_WAIT_PERIOD
+}heartbeatStates_t;
+
+static heartbeatStates_t currentState = STATE_TURN_LED_ON;
 
 void Heartbeat_run(uint16_t dutyCycle, uint16_t period)
 {
+  heartbeatStates_t nextState = currentState;
+  uint8_t isItTime = 0;
+  static uint32_t startTime = 0;
+
+  switch(currentState) {
+  case STATE_TURN_LED_ON:
+    drvLeds_on(LED_array);
+    startTime = ulSysTime_getCurrentTime();
+    nextState = STATE_WAIT_DUTYCYCLE;
+    break;
+  case STATE_WAIT_DUTYCYCLE:
+    isItTime = ulSysTime_isItTime(startTime, dutyCycle);
+    if (isItTime) {
+      nextState = STATE_TURN_LED_OFF;
+    }
+    break;
+  case STATE_TURN_LED_OFF:
+    drvLeds_off(LED_array);
+    startTime = ulSysTime_getCurrentTime();
+    nextState = STATE_WAIT_PERIOD;
+    break;
+  case STATE_WAIT_PERIOD:
+    isItTime = ulSysTime_isItTime(startTime, period - dutyCycle);
+    if (isItTime) {
+      nextState = STATE_TURN_LED_ON;
+    }
+    break;
+  default:
+    nextState = STATE_TURN_LED_ON;
+  }
+
+  currentState = nextState;
+/*
   for (uint16_t ledName = 0; ledName < LED_AMOUNT - 1 ; ledName++) {
   drvLeds_on(LED_array[ledName]);
   HAL_Delay(dutyCycle);
   drvLeds_off(LED_array[ledName]);
   HAL_Delay(period - dutyCycle);
   }
+*/
 }
