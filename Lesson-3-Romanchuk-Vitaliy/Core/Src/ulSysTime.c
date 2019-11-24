310067040b3c6ccff85bbff37e46f0fee36bedca
Romanchuk.Vitaliy <acforSN@gmail.com>
2019-11-24 19:47:08 +0200

    Lesson #3: Create new utility
    ulSysTime utility was created for STM32F4DISCOVERY LEDs and ulSysTime.h and
    ulSysTime.c files was created and added to new project.
    
    Signed-off-by: Romanchuk.Vitaliy <acforSN@gmail.com>

---
+++ Lesson-3-Romanchuk-Vitaliy/Core/Src/ulSysTime.c
@@ -0,0 +1,27 @@
+/*
+ * ulSysTime.c
+ *
+ *  Created on: 24 лист. 2019 р.
+ *      Author: Romanchuk.Vitaliy
+ */
+
+#include "stdint.h"
+#include "stdlib.h"
+#include "ulSysTime.h"
+
+uint32_t ulSysTime_getCurrentTime() {
+	return 0;
+}
+
+uint8_t ulSysTime_isItTime(uint32_t startTime, uint32_t delay) {
+	uint32_t currentTime = ulSysTime_getCurrentTime;
+	uint8_t returnValue = 0;
+
+	if (abs (currentTime - startTime) > delay) {
+		returnValue = 1;
+	}
+
+	return returnValue;
+}
+
+
