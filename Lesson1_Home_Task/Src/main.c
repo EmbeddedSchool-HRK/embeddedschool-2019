ce2f11383f7fccfd27d5f1b028578bf5cf8b921f
Viacheslav Olizarenko <stiwenss@gmail.com>
2019-12-02 00:32:15 +0200

    Lesson#2: hearbeat and rainbow are applied in main.c
    hearbeat and rainbow function are added to while(1) in main.c
    
    Signed-off-by: Viacheslav Olizarenko <stiwenss@gmail.com>

--- Lesson1_Home_Task/Src/main.c
+++ Lesson1_Home_Task/Src/main.c
@@ -21,6 +21,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usb_host.h"
+#include "heartbeat.h"
+#include "rainbow.h"
+#include "drvLeds.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -34,6 +37,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define PULSE_DELAY 500
+#define DUTY_CYCLE  500
+#define PERIOD  1000
+#define LEDS_NUMBER 3
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -61,9 +68,6 @@ static void MX_SPI1_Init(void);
 void MX_USB_HOST_Process(void);
 
 /* USER CODE BEGIN PFP */
- void HeartBeat();
- void Start_Blink();
- void Infinite_Blink();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -78,7 +82,12 @@ void MX_USB_HOST_Process(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+list_t LEDS_sequence[LEDS_NUMBER] =
+{
+  LED_BLUE,
+  LED_GREEN,
+  LED_ORANGE
+};
   /* USER CODE END 1 */
   
 
@@ -105,18 +114,20 @@ int main(void)
   MX_SPI1_Init();
   MX_USB_HOST_Init();
   /* USER CODE BEGIN 2 */
-  Start_Blink(1000);
+  
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    heartbeat(DUTY_CYCLE, PERIOD);
+    rainbow(PULSE_DELAY,LEDS_sequence, LEDS_NUMBER);
     /* USER CODE END WHILE */
     MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
-  Infinite_Blink(500);
+  
 		
   }
   /* USER CODE END 3 */
@@ -371,39 +382,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void HeartBeat(uint32_t period, uint32_t Duty_Cycle){
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-  HAL_Delay(Duty_Cycle);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-  HAL_Delay(period-Duty_Cycle);
-}
-
-void Infinite_Blink(uint32_t Inf_time){
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-  HAL_Delay(Inf_time);
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
-  HAL_Delay(Inf_time);
-  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
-  HAL_Delay(Inf_time);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-  HAL_Delay(Inf_time);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-}
- 
-void Start_Blink(uint32_t Start_time){ 
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
-  HAL_Delay(Start_time);
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-}
 
 /* USER CODE END 4 */
 
