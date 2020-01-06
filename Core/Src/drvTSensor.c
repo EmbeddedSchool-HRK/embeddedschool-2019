791e6673ce663e8866b37c17d3f20fdfadd3680a
S1ckret <S1ckret@users.noreply.github.com>
2020-01-06 20:37:47 +0200

    Lesson 5: Add temperature filter
    We took 12 measurements, drop extremums, divide by 10.
    
    Signed-off-by: S1ckret <S1ckret@users.noreply.github.com>

--- Core/Src/drvTSensor.c
+++ Core/Src/drvTSensor.c
@@ -14,8 +14,13 @@
 #define TEMP_30 30
 #define TEMP_110 110
 
+#define MEASUREMENT_COUNT 12
+#define CORRECT_DATA_COUNT 10
+
 static float slope = 0.f;
 static float temperature = 0.f;
+
+static float getAverageMeasurement(ADC_HandleTypeDef* ptr_adc);
 
 void drvTSenosr_init()
 {
@@ -31,8 +36,33 @@ void drvTSensor_update(ADC_HandleTypeDef* ptr_adc)
 {
 	HAL_ADC_Start(ptr_adc);
 	HAL_ADC_PollForConversion(ptr_adc, 100);
-	uint16_t raw_data_adc = HAL_ADC_GetValue(ptr_adc);
+	uint16_t raw_data_adc = getAverageMeasurement(ptr_adc);
 	HAL_ADC_Stop(ptr_adc);
 
 	temperature = (raw_data_adc - (float)(*TS_CAL_VAL_30)) * slope + TEMP_30;
+}
+
+
+//	Make 12 measurements.
+//	Drop max and min.
+//	Evaluate average of 10 left.
+static float getAverageMeasurement(ADC_HandleTypeDef* ptr_adc)
+{
+	uint16_t data_arr[MEASUREMENT_COUNT];
+	uint16_t max = 0;
+	uint16_t min = UINT16_MAX;
+	uint32_t sum = 0;
+
+	for (uint8_t i = 0; i < MEASUREMENT_COUNT; i++)
+	{
+		data_arr[i] = HAL_ADC_GetValue(ptr_adc);
+		if (data_arr[i] > max) max = data_arr[i];
+		else if (data_arr[i] < min) min = data_arr[i];
+
+		sum += data_arr[i];
+	}
+	sum = sum - min - max;
+
+	float average_raw_data = (float)(sum) / (float)CORRECT_DATA_COUNT;
+	return average_raw_data;
 }
